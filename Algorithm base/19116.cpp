136b9e2f609d386a9dd2d433df49c673b146b96b
Viper3 <41693046+ClearVIper3@users.noreply.github.com>
2025-04-15 00:08:01 +0800

    Update 19116.cpp

--- Algorithm base/19116.cpp
+++ Algorithm base/19116.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
@@ -6,15 +7,15 @@ int main()
 {
     int T;
     cin >> T;
-    int *ugly = new int[100000001] ;
+    vector<int> ugly(1000001);
     ugly[0] = 1;
     int a = 0,b = 0,c = 0;
-    for(int i = 1; i < 100000001; i++)
+    for(int i = 1; i < 1000001; i++)
     {
         int num1 = ugly[a] * 2;
         int num2 = ugly[b] * 3;
         int num3 = ugly[c] * 5;
-        ugly[i] = min(ugly[a] * 2,min(ugly[b] * 3,ugly[c] * 5));
+        ugly[i] = min(num1,min(num2,num3));
         if(ugly[i] == num1) a++;
         if(ugly[i] == num2) b++;
         if(ugly[i] == num3) c++;
@@ -27,6 +28,5 @@ int main()
         cout << ugly[n-1] << endl;
     }
 
-    delete [] ugly;
     return 0;
 }
