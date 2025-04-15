c061aeb20275c287a0278d64e1d2fbb6b3ec0ff6
Viper3 <41693046+ClearVIper3@users.noreply.github.com>
2025-04-15 15:04:33 +0800

    Create 8623.cpp

---
+++ Algorithm base/8623.cpp
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "math.h"
+
+long func(int n)
+{
+    long s=0;
+    int i, q;
+    if(n<1) return 0;
+    q= sqrt(n);
+    for(i=1; i<=q; i++)
+        s+=(n/i-n/(i+1))*i;
+    for(i=1; i<=n/(q+1); i++)
+        s+=n/i;
+ 
+    return s;
+}    
+
+main()
+{
+    int a;
+    int n;
+    scanf("%d", &n);
+    while(n--)
+    {
+        scanf("%d", &a);
+        printf("%ld\n", func(a));
+    }    
+}    
+
+		
